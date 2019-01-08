34c55a027881abee704097f53be69d6a0b1897ee
odzk <ody@web-mech.net>
2019-01-08 16:42:47 +0800

    updated user display in author box

--- templates/author_box_top.php
+++ templates/author_box_top.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-md-5 col-xs-12 text-center">
 		<img src="<?php echo get_avatar_url( get_the_author_meta( 'ID' )); ?>" style="border-radius: 100%; vertical-align: middle;">
-		<a href="/author/<?php echo get_the_author_meta('display_name'); ?>" style="margin-left: 10px;"><?php echo the_author(); ?></a>
+		<a href="/author/<?php echo get_the_author_meta('user_nicename'); ?>" style="margin-left: 10px;"><?php echo the_author(); ?></a>
 	</div>
 	<div class="col-md-12" style="margin-top: 20px;">
 		<?php get_template_part( 'sns' );?>
